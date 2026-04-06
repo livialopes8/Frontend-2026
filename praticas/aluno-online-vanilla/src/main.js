0faa4c61a36b17a457f979e5a13eecd0ce2794ee
livialopes8 <liviasuper885@gmail.com>
2026-04-06 09:34:17 -0300

    feat: conclui pratica02. Fecha #4

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,71 @@
 import './style.css'
+import './style.css'
+
+const form = document.querySelector('#login-form')
+
+if (form) {
+  const emailInput = document.querySelector('#email')
+  const senhaInput = document.querySelector('#senha')
+
+  const emailError = document.querySelector('#email-error')
+  const senhaError = document.querySelector('#senha-error')
+  const formMessage = document.querySelector('#form-message')
+
+  function limparErros() {
+    emailError.textContent = ''
+    senhaError.textContent = ''
+    formMessage.textContent = ''
+  }
+
+  function validarFormulario() {
+    let formularioValido = true
+
+    limparErros()
+
+    if (emailInput.value.trim() === '') {
+      emailError.textContent = 'Informe o e-mail.'
+      formularioValido = false
+    }
+
+    if (senhaInput.value.trim() === '') {
+      senhaError.textContent = 'Informe a senha.'
+      formularioValido = false
+    }
+
+    if (!formularioValido) {
+      formMessage.textContent = 'Preencha os campos obrigatórios.'
+    }
+
+    return formularioValido
+  }
+
+  emailInput.addEventListener('input', () => {
+    if (emailInput.value.trim() !== '') {
+      emailError.textContent = ''
+    }
+
+    if (emailInput.value.trim() !== '' && senhaInput.value.trim() !== '') {
+      formMessage.textContent = ''
+    }
+  })
+
+  senhaInput.addEventListener('input', () => {
+    if (senhaInput.value.trim() !== '') {
+      senhaError.textContent = ''
+    }
+
+    if (emailInput.value.trim() !== '' && senhaInput.value.trim() !== '') {
+      formMessage.textContent = ''
+    }
+  })
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const formularioValido = validarFormulario()
+
+    if (formularioValido) {
+      window.location.href = '/index.html'
+    }
+  })
+}
